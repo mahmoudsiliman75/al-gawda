9b5a5443029554f6ec76737ce2a593386458e8e7
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-02-10 17:18:56 +0200

    create team member card

--- src/components/TeamWork.vue
+++ src/components/TeamWork.vue
@@ -1,9 +1,126 @@
 <template>
   <div class="teamwork-sec">
-    <h1> team work sec </h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h2 class="sec-header"> Our Team</h2>
+        </div>
 
-    <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-      <path fill="#FF0066" d="M43.5,-76.6C54.5,-68.9,60.4,-53.6,67.8,-39.6C75.1,-25.5,83.9,-12.8,85.8,1.1C87.7,15,82.8,30,72.8,39.5C62.9,49,47.9,53,34.9,59.2C21.9,65.5,11,74,-0.8,75.3C-12.5,76.6,-25,70.7,-37.3,64.1C-49.6,57.5,-61.8,50.1,-71,39.3C-80.3,28.4,-86.7,14.2,-87.1,-0.2C-87.5,-14.7,-82,-29.4,-72.8,-40.4C-63.6,-51.4,-50.8,-58.7,-38.1,-65.3C-25.4,-72,-12.7,-78.1,1.8,-81.2C16.2,-84.2,32.4,-84.2,43.5,-76.6Z" transform="translate(100 100)" />
-    </svg>
+        <div class="col-12 col-md-4 px-2">
+          <div class="member-card mb-2 d-flex flex-column justify-content-center align-items-center">
+            <!-- START:: FIRST SVG -->
+            <svg class="first" viewBox="0 0 250 250" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20.3,-30.2C30.1,-29.4,44.5,-31.7,56.2,-27.2C67.8,-22.7,76.6,-11.4,80.5,2.2C84.3,15.8,83.2,31.6,71.7,36.3C60.2,41,38.2,34.6,24.5,42.2C10.8,49.9,5.4,71.6,-3.9,78.3C-13.2,85,-26.3,76.7,-31.6,64.2C-36.9,51.7,-34.2,35,-41.9,23.6C-49.7,12.1,-67.8,6.1,-75.9,-4.7C-84.1,-15.5,-82.4,-31,-72.5,-38.7C-62.6,-46.4,-44.6,-46.2,-31.2,-44.9C-17.7,-43.5,-8.9,-41.1,-1.8,-37.9C5.2,-34.7,10.4,-30.9,20.3,-30.2Z" transform="translate(100 100)" />
+            </svg>
+            <!-- END:: FIRST SVG -->
+
+            <img src="https://cdn.powerpackelements.com/wp-content/uploads/2017/11/Team-memeber-01.png" alt="Team Member Img">
+
+            <h4> Mohamed Eid </h4>
+            <h6> Back-end Developer </h6>
+
+            <ul class="list-unstyled">
+              <li> 
+                <a href="#"> <b-icon-facebook /> </a> 
+              </li>
+
+              <li> 
+                <a href="#"> <b-icon-twitter /> </a> 
+              </li>
+
+              <li> 
+                <a href="#"> <b-icon-linkedin /> </a> 
+              </li>
+            </ul>
+
+            <!-- START:: LAST SVG -->
+            <svg  class="last" viewBox="0 0 250 250" xmlns="http://www.w3.org/2000/svg">
+              <path d="M20.3,-30.2C30.1,-29.4,44.5,-31.7,56.2,-27.2C67.8,-22.7,76.6,-11.4,80.5,2.2C84.3,15.8,83.2,31.6,71.7,36.3C60.2,41,38.2,34.6,24.5,42.2C10.8,49.9,5.4,71.6,-3.9,78.3C-13.2,85,-26.3,76.7,-31.6,64.2C-36.9,51.7,-34.2,35,-41.9,23.6C-49.7,12.1,-67.8,6.1,-75.9,-4.7C-84.1,-15.5,-82.4,-31,-72.5,-38.7C-62.6,-46.4,-44.6,-46.2,-31.2,-44.9C-17.7,-43.5,-8.9,-41.1,-1.8,-37.9C5.2,-34.7,10.4,-30.9,20.3,-30.2Z" transform="translate(100 100)" />
+            </svg>
+            <!-- END:: LAST SVG -->
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+// START:: IMPORT MAIN FILE
+@import "../assets/sass/main.scss";
+// END:: IMPORT MAIN FILE
+
+.teamwork-sec {
+  padding: $sectionPadding;
+  .member-card  {
+    position: relative;
+    border: 1px solid #e1e1e1;
+    overflow: hidden;
+    @include borderRadius(10px);
+    height: 350px;
+    svg {
+      &.first {
+        position: absolute;
+        top: -250px;
+        right: -250px;
+        z-index: -1;
+        transform: rotate(90deg);
+        path {
+          fill: $mainColor;
+        }
+      }
+      &.last {
+        position: absolute;
+        bottom: -295px;
+        left: -196px;
+        z-index: -1;
+        transform: rotate(259deg);
+        path {
+          fill: $mainColor;
+        }
+      }
+    }
+    img {
+      min-width: 150px;
+      max-width: 150px;
+      min-height: 150px;
+      max-height: 150px;
+      margin-bottom: 15px;
+      border: 3px solid $mainColor;
+      @include borderRadius(50%);
+    }
+    ul {
+      margin-bottom: 0;
+      padding: 8px;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      display: block;
+      width: 100%;
+      background-color: rgba(0,0,0,0.4);
+      display: flex;
+      justify-content: center;
+      li {
+        margin: 0 8px;
+        transform: scale(1.1);
+        a {
+          text-decoration: none;
+          color: #fff;
+          font-size: 22px;
+          @include transitioning();
+          &:hover {
+            color: $secondryColor;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
